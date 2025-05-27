1bdb234791e40019eb4f06031f06f3f597abd3ca
justinmhudson <111338167+justinmhudson@users.noreply.github.com>
2025-05-26 17:57:59 -0700

    Update Bayes.py

--- Justin Hudson/Bayes.py
+++ Justin Hudson/Bayes.py
@@ -35,7 +35,10 @@ for review in negative:
         negative_stemmed.append(stemmer.stem(word))
 
 while True:
-    user_input = input("\nEnter word: ")
+    user_input = input("\nEnter a word (\"stop\" to end program): ")
+
+    if user_input.lower() == "stop":
+        break
 
     user_input_stemmed = stemmer.stem(user_input)
 
@@ -58,19 +61,17 @@ while True:
             count_total_negative += 1
     p_input = (count_input_positive + count_input_negative) / (count_total_positive + count_total_negative)
     if p_input != 0:
-        break
+        # Calculate P(positive)
+        p_positive = len(positive) / (len(positive) + len(negative))
+
+        # Calculate P("input"|positive)
+        p_input_given_positive = (count_input_positive) / (count_total_positive)
+
+        # Calculate P(positive|"input")
+        # Bayes Theorem:
+        # P(A|B) = P(B|A)P(A) / P(B)
+        p_positive_given_input = p_input_given_positive * p_positive / p_input
+
+        print(f"The probability of a review being positive given it contains the word \"{user_input}\" is {p_positive_given_input * 100}%")
     else:
         print("That word does not appear in the corpus.")
-
-# Calculate P(positive)
-p_positive = len(positive) / (len(positive) + len(negative))
-
-# Calculate P("input"|positive)
-p_input_given_positive = (count_input_positive) / (count_total_positive)
-
-# Calculate P(positive|"input")
-# Bayes Theorem:
-# P(A|B) = P(B|A)P(A) / P(B)
-p_positive_given_input = p_input_given_positive * p_positive / p_input
-
-print(f"The probability of a review being positive given it contains the word \"{user_input}\" is {p_positive_given_input * 100}%")
